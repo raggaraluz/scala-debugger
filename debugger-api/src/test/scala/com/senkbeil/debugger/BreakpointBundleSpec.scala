1a2d226839c2b68e58f5da912f036dab028a1af6
Chip Senkbeil <chip.senkbeil@gmail.com>
2015-04-26 01:13:31 -0500

    Added tests for breakpoint bundle

--- debugger-api/src/test/scala/com/senkbeil/debugger/BreakpointBundleSpec.scala
+++ debugger-api/src/test/scala/com/senkbeil/debugger/BreakpointBundleSpec.scala
@@ -1,5 +1,6 @@
 package com.senkbeil.debugger
 
+import com.sun.jdi.{VirtualMachine, ThreadReference, ObjectReference}
 import com.sun.jdi.request.BreakpointRequest
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{BeforeAndAfter, Matchers, FunSpec}
@@ -11,7 +12,7 @@ class BreakpointBundleSpec extends FunSpec with Matchers with BeforeAndAfter
   private var breakpointBundle: BreakpointBundle = _
 
   before {
-    breakpointRequests = Seq(stub[BreakpointRequest], stub[BreakpointRequest])
+    breakpointRequests = Seq(mock[BreakpointRequest], mock[BreakpointRequest])
     breakpointBundle = new BreakpointBundle(breakpointRequests)
   }
 
@@ -36,105 +37,234 @@ class BreakpointBundleSpec extends FunSpec with Matchers with BeforeAndAfter
 
     describe("#length") {
       it("should return the total number of breakpoint requests contained") {
-        fail()
+        val expected = breakpointRequests.length
+        val actual = breakpointBundle.length
+
+        actual should be (expected)
       }
     }
 
     describe("#iterator") {
       it("should return an iterator over the contained breakpoint requests") {
-        fail()
+        val iterator = breakpointBundle.iterator
+
+        while (iterator.hasNext)
+          breakpointRequests should contain (iterator.next())
       }
     }
 
     describe("#addInstanceFilter") {
       it("should add the instance filter to each underlying breakpoint request") {
-        fail()
+        val instance = stub[ObjectReference]
+
+        breakpointRequests.foreach(request =>
+          (request.addInstanceFilter _).expects(instance))
+
+        breakpointBundle.addInstanceFilter(instance)
       }
     }
 
     describe("#addThreadFilter") {
       it("should add the thread filter to each underlying breakpoint request") {
-        fail()
+        val threadReference = stub[ThreadReference]
+
+        breakpointRequests.foreach(request =>
+          (request.addThreadFilter _).expects(threadReference))
+
+        breakpointBundle.addThreadFilter(threadReference)
       }
     }
 
     describe("#addCountFilter") {
       it("should add the count filter to each underlying breakpoint request") {
-        fail()
+        val count = 3
+
+        breakpointRequests.foreach(request =>
+          (request.addCountFilter _).expects(count))
+
+        breakpointBundle.addCountFilter(count)
       }
     }
 
     describe("#disable") {
       it("should disable each underlying breakpoint request") {
-        fail()
+        breakpointRequests.foreach(request =>
+          (request.disable _).expects())
+
+        breakpointBundle.disable()
       }
     }
 
     describe("#enable") {
       it("should enable each underlying breakpoint request") {
-        fail()
+        breakpointRequests.foreach(request =>
+          (request.enable _).expects())
+
+        breakpointBundle.enable()
       }
     }
 
     describe("#isEnabled") {
       it("should return true if all breakpoint requests are enabled") {
-        fail()
+        val expected = true
+        breakpointRequests.foreach(request =>
+          (request.isEnabled _).expects().returns(expected))
+
+        val actual = breakpointBundle.isEnabled
+
+        actual should be (expected)
       }
 
       it("should return false if all breakpoint requests are disabled") {
-        fail()
+        val expected = false
+        breakpointRequests.foreach(request =>
+          (request.isEnabled _).expects().returns(expected))
+
+        val actual = breakpointBundle.isEnabled
+
+        actual should be (expected)
       }
 
       it("should throw an exception if the breakpoint requests are out of sync") {
-        fail()
+        val indexedBreakpointRequests = breakpointRequests.zipWithIndex
+
+        indexedBreakpointRequests.filter(_._2 % 2 == 0).map(_._1)
+          .foreach(request => (request.isEnabled _).expects().returns(false))
+
+        indexedBreakpointRequests.filter(_._2 % 2 != 0).map(_._1)
+          .foreach(request => (request.isEnabled _).expects().returns(true))
+
+        // Out of sync, so should throw an exception
+        intercept[AssertionError] {
+          breakpointBundle.isEnabled
+        }
       }
     }
 
     describe("#setEnabled") {
       it("should set the enable status of each underlying breakpoint request") {
-        fail()
+        val expected = true
+        breakpointRequests.foreach(request =>
+          (request.setEnabled _).expects(expected))
+
+        breakpointBundle.setEnabled(expected)
       }
     }
 
     describe("#getProperty") {
       it("should return the property contained by all breakpoint requests") {
-        fail()
+        val (key, value) = (mock[AnyRef], mock[AnyRef])
+        val expected = value
+
+        breakpointRequests.foreach(request =>
+          (request.getProperty _).expects(key).returns(value))
+
+        val actual = breakpointBundle.getProperty(key)
+
+        actual should be (expected)
       }
 
       it("should throw an exception if the breakpoint requests are out of sync") {
-        fail()
+        val (key, value1, value2) = (mock[AnyRef], mock[AnyRef], mock[AnyRef])
+        val indexedBreakpointRequests = breakpointRequests.zipWithIndex
+
+        indexedBreakpointRequests.filter(_._2 % 2 == 0).map(_._1).foreach(
+          request => (request.getProperty _).expects(key).returns(value1))
+
+        indexedBreakpointRequests.filter(_._2 % 2 != 0).map(_._1).foreach(
+          request => (request.getProperty _).expects(key).returns(value2))
+
+        // Out of sync, so should throw an exception
+        intercept[AssertionError] {
+          breakpointBundle.getProperty(key)
+        }
       }
     }
 
     describe("#putProperty") {
       it("should set the property for each underlying breakpoint request") {
-        fail()
+        val (key, value) = (mock[AnyRef], mock[AnyRef])
+        breakpointRequests.foreach(request =>
+          (request.putProperty _).expects(key, value))
+
+        breakpointBundle.putProperty(key, value)
       }
     }
 
     describe("#setSuspendPolicy") {
       it("should set the suspend policy for each underlying breakpoint request") {
-        fail()
+        val policy = 999
+        breakpointRequests.foreach(request =>
+          (request.setSuspendPolicy _).expects(policy))
+
+        breakpointBundle.setSuspendPolicy(policy)
       }
     }
 
     describe("#suspendPolicy") {
       it("should return the suspend policy matching all of the underlying breakpoint requests") {
-        fail()
+        val expected = 3
+
+        breakpointRequests.foreach(request =>
+          (request.suspendPolicy _).expects().returns(expected))
+
+        val actual = breakpointBundle.suspendPolicy
+
+        actual should be (expected)
       }
 
       it("should throw an exception if the breakpoint requests are out of sync") {
-        fail()
+        val indexedBreakpointRequests = breakpointRequests.zipWithIndex
+        val (suspendPolicy1, suspendPolicy2) = (0, 1)
+
+        indexedBreakpointRequests.filter(_._2 % 2 == 0)
+          .map(_._1).foreach(request =>
+            (request.suspendPolicy _).expects().returns(suspendPolicy1)
+          )
+
+        indexedBreakpointRequests.filter(_._2 % 2 != 0)
+          .map(_._1).foreach(request =>
+            (request.suspendPolicy _).expects().returns(suspendPolicy2)
+          )
+
+        // Out of sync, so should throw an exception
+        intercept[AssertionError] {
+          breakpointBundle.suspendPolicy
+        }
       }
     }
 
     describe("#virtualMachine") {
       it("should return the virtual machine where all of the underlying breakpoint requests reside") {
-        fail()
+        val expected = mock[VirtualMachine]
+
+        breakpointRequests.foreach(request =>
+          (request.virtualMachine _).expects().returns(expected))
+
+        val actual = breakpointBundle.virtualMachine
+
+        actual should be (expected)
       }
 
       it("should throw an exception if the breakpoint requests are out of sync") {
-        fail()
+        val indexedBreakpointRequests = breakpointRequests.zipWithIndex
+        val (virtualMachine1, virtualMachine2) =
+          (mock[VirtualMachine], mock[VirtualMachine])
+
+        indexedBreakpointRequests.filter(_._2 % 2 == 0)
+          .map(_._1).foreach(request =>
+            (request.virtualMachine _).expects().returns(virtualMachine1)
+          )
+
+        indexedBreakpointRequests.filter(_._2 % 2 != 0)
+          .map(_._1).foreach(request =>
+            (request.virtualMachine _).expects().returns(virtualMachine2)
+          )
+
+        // Out of sync, so should throw an exception
+        intercept[AssertionError] {
+          breakpointBundle.virtualMachine
+        }
       }
     }
   }
