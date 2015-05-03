389d3209f00840196d395bedf27cdf5dde800305
Chip Senkbeil <chip.senkbeil@gmail.com>
2015-05-03 14:37:46 -0500

    Added helper method to JDI that suspends a thread and executes code

--- debugger-api/src/main/scala/com/senkbeil/debugger/jdi/JDIHelperMethods.scala
+++ debugger-api/src/main/scala/com/senkbeil/debugger/jdi/JDIHelperMethods.scala
@@ -11,23 +11,50 @@ trait JDIHelperMethods extends LogLike {
 
   /**
    * Executes the provided block of code during the state of a suspended
-   * virtual machine. This method is synchronized to prevent another thread
-   * suspending the same virtual machine.
+   * virtual machine. The virtual machine is synchronized to prevent another
+   * thread suspending the same virtual machine.
    *
    * @param thunk The block of code to execute
    * @tparam T The type of result from the block of code
    *
    * @return The results of attempting to execute the block of code
    */
-  protected def suspendVirtualMachineAndExecute[T](thunk: => T) = synchronized {
-    // Pause the virtual machine while we perform the operation
-    _virtualMachine.suspend()
+  protected def suspendVirtualMachineAndExecute[T](thunk: => T) =
+    _virtualMachine.synchronized {
+      // Pause the virtual machine while we perform the operation
+      _virtualMachine.suspend()
+
+      // Execute the operation
+      val result = Try(thunk)
+
+      // Resume the virtual machine now that the operation has been completed
+      _virtualMachine.resume()
+
+      // Return the result of the operation
+      result
+    }
+
+  /**
+   * Executes the provided block of code during the state of a suspended
+   * thread. The thread reference is synchronized to prevent another thread
+   * suspending the same thread reference.
+   *
+   * @param thunk The block of code to execute
+   * @tparam T The type of result from the block of code
+   *
+   * @return The results of attempting to execute the block of code
+   */
+  protected def suspendThreadAndExecute[T](
+    threadReference: ThreadReference
+  )(thunk: => T): Try[T] = threadReference.synchronized {
+    // Pause the thread while we perform the operation
+    threadReference.suspend()
 
     // Execute the operation
     val result = Try(thunk)
 
-    // Resume the virtual machine now that the operation has been completed
-    _virtualMachine.resume()
+    // Resume the thread now that the operation has been completed
+    threadReference.resume()
 
     // Return the result of the operation
     result
