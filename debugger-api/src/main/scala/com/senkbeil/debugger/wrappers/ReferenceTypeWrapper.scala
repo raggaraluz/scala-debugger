811dbf85afd36bec6ef5964f9a8efe3f96b22390
Chip Senkbeil <chip.senkbeil@gmail.com>
2015-05-03 00:21:19 -0500

    Added tests for reference type

--- debugger-api/src/main/scala/com/senkbeil/debugger/wrappers/ReferenceTypeWrapper.scala
+++ debugger-api/src/main/scala/com/senkbeil/debugger/wrappers/ReferenceTypeWrapper.scala
@@ -14,15 +14,26 @@ import scala.util.Try
 class ReferenceTypeWrapper(private val _referenceType: ReferenceType)
   extends LogLike
 {
+  require(_referenceType != null, "Reference type cannot be null!")
+
   /**
    * Retrieves the static fields and their values.
    *
    * @return The list of static fields and their respective values
    */
   def staticFieldsAndValues: Seq[(Field, Value)] =
-    Try(_referenceType.allFields()).map(_.asScala)
-      .getOrElse(Nil)
+    staticFields
       .map(field => (field, Try(_referenceType.getValue(field))))
       .filter(_._2.isSuccess)
       .map(t => (t._1, t._2.get))
+
+  /**
+   * Retrieves the static fields.
+   *
+   * @return The list of static fields
+   */
+  def staticFields: Seq[Field] =
+    Try(_referenceType.allFields()).map(_.asScala)
+      .getOrElse(Nil)
+      .filter(_.isStatic)
 }
