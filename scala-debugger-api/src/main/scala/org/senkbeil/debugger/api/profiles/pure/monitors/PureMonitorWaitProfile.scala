f93da604a1362a79cb994494e2c78c439d915455
Chip Senkbeil <chip.senkbeil@gmail.com>
2015-11-25 20:50:48 -0600

    Added pure MonitorWait profile request removal

--- scala-debugger-api/src/main/scala/org/senkbeil/debugger/api/profiles/pure/monitors/PureMonitorWaitProfile.scala
+++ scala-debugger-api/src/main/scala/org/senkbeil/debugger/api/profiles/pure/monitors/PureMonitorWaitProfile.scala
@@ -1,5 +1,8 @@
 package org.senkbeil.debugger.api.profiles.pure.monitors
 
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
+
 import com.sun.jdi.event.MonitorWaitEvent
 import org.senkbeil.debugger.api.lowlevel.JDIArgument
 import org.senkbeil.debugger.api.lowlevel.events.{EventManager, JDIEventArgument}
@@ -8,11 +11,13 @@ import org.senkbeil.debugger.api.lowlevel.monitors.MonitorWaitManager
 import org.senkbeil.debugger.api.lowlevel.requests.JDIRequestArgument
 import org.senkbeil.debugger.api.lowlevel.requests.properties.UniqueIdProperty
 import org.senkbeil.debugger.api.lowlevel.utils.JDIArgumentGroup
+import org.senkbeil.debugger.api.pipelines.Pipeline
 import org.senkbeil.debugger.api.pipelines.Pipeline.IdentityPipeline
 import org.senkbeil.debugger.api.profiles.traits.monitors.MonitorWaitProfile
 import org.senkbeil.debugger.api.utils.Memoization
 import org.senkbeil.debugger.api.lowlevel.events.EventType._
 
+import scala.collection.JavaConverters._
 import scala.util.Try
 
 /**
@@ -24,12 +29,21 @@ trait PureMonitorWaitProfile extends MonitorWaitProfile {
   protected val eventManager: EventManager
 
   /**
+   * Contains mapping from input to a counter indicating how many pipelines
+   * are currently active for the input.
+   */
+  private val pipelineCounter = new ConcurrentHashMap[
+    Seq[JDIArgument],
+    AtomicInteger
+  ]().asScala
+
+  /**
    * Constructs a stream of monitor wait events.
    *
    * @param extraArguments The additional JDI arguments to provide
    *
-   * @return The stream of monitor wait events and any retrieved
-   *         data based on requests from extra arguments
+   * @return The stream of monitor wait events and any retrieved data based on
+   *         requests from extra arguments
    */
   override def onMonitorWaitWithData(
     extraArguments: JDIArgument*
@@ -40,10 +54,10 @@ trait PureMonitorWaitProfile extends MonitorWaitProfile {
   }
 
   /**
-   * Creates a new monitor wait request using the given arguments.
-   * The request is memoized, meaning that the same request will be returned
-   * for the same arguments. The memoized result will be thrown out if the
-   * underlying request storage indicates that the request has been removed.
+   * Creates a new monitor wait request using the given arguments. The request
+   * is memoized, meaning that the same request will be returned for the same
+   * arguments. The memoized result will be thrown out if the underlying
+   * request storage indicates that the request has been removed.
    *
    * @return The id of the created monitor wait request
    */
@@ -57,7 +71,8 @@ trait PureMonitorWaitProfile extends MonitorWaitProfile {
         val requestId = newMonitorWaitRequestId()
         val args = UniqueIdProperty(id = requestId) +: input
 
-        monitorWaitManager.createMonitorWaitRequest(
+        monitorWaitManager.createMonitorWaitRequestWithId(
+          requestId,
           args: _*
         ).get
 
@@ -70,17 +85,17 @@ trait PureMonitorWaitProfile extends MonitorWaitProfile {
         !monitorWaitManager.monitorWaitRequestList
           .flatMap(monitorWaitManager.getMonitorWaitRequestArguments)
           .map(_.filterNot(_.isInstanceOf[UniqueIdProperty]))
-          .exists(_ == key)
+          .contains(key)
       }
     )
   }
 
   /**
-   * Creates a new pipeline of monitor wait events and data using
-   * the given arguments. The pipeline is NOT memoized; therefore, each call
-   * creates a new pipeline with a new underlying event handler feeding the
-   * pipeline. This means that the pipeline needs to be properly closed to
-   * remove the event handler.
+   * Creates a new pipeline of monitor wait events and data using the given
+   * arguments. The pipeline is NOT memoized; therefore, each call creates a
+   * new pipeline with a new underlying event handler feeding the pipeline.
+   * This means that the pipeline needs to be properly closed to remove the
+   * event handler.
    *
    * @param requestId The id of the request whose events to stream through the
    *                  new pipeline
@@ -91,12 +106,49 @@ trait PureMonitorWaitProfile extends MonitorWaitProfile {
   protected def newMonitorWaitPipeline(
     requestId: String,
     args: Seq[JDIEventArgument]
-    ): IdentityPipeline[MonitorWaitEventAndData] = {
+  ): IdentityPipeline[MonitorWaitEventAndData] = {
     val eArgsWithFilter = UniqueIdPropertyFilter(id = requestId) +: args
-    eventManager.addEventDataStream(
-      MonitorWaitEventType,
-      eArgsWithFilter: _*
-    ).map(t => (t._1.asInstanceOf[MonitorWaitEvent], t._2)).noop()
+    val newPipeline = eventManager
+      .addEventDataStream(MonitorWaitEventType, eArgsWithFilter: _*)
+      .map(t => (t._1.asInstanceOf[MonitorWaitEvent], t._2))
+      .noop()
+
+    // Create a companion pipeline who, when closed, checks to see if there
+    // are no more pipelines for the given request and, if so, removes the
+    // request as well
+    val closePipeline = Pipeline.newPipeline(
+      classOf[MonitorWaitEventAndData],
+      newMonitorWaitPipelineCloseFunc(requestId, args)
+    )
+
+    // Increment the counter for open pipelines
+    pipelineCounter
+      .getOrElseUpdate(args, new AtomicInteger(0))
+      .incrementAndGet()
+
+    val combinedPipeline = newPipeline.unionOutput(closePipeline)
+    combinedPipeline
+  }
+
+  /**
+   * Creates a new function used for closing generated pipelines.
+   *
+   * @param requestId The id of the request
+   * @param args The arguments associated with the request
+   *
+   * @return The new function for closing the pipeline
+   */
+  protected def newMonitorWaitPipelineCloseFunc(
+    requestId: String,
+    args: Seq[JDIEventArgument]
+  ): () => Unit = () => {
+    val pCounter = pipelineCounter(args)
+
+    val totalPipelinesRemaining = pCounter.decrementAndGet()
+
+    if (totalPipelinesRemaining == 0) {
+      monitorWaitManager.removeMonitorWaitRequest(requestId)
+    }
   }
 
   /**
