ef04b3838d00f36b07e6fc4df56c8508f2abfcd0
Chip Senkbeil <chip.senkbeil@gmail.com>
2015-11-27 23:55:20 -0600

    Added MultiMap to use with low-level managers

---
+++ scala-debugger-api/src/main/scala/org/senkbeil/debugger/api/utils/MultiMap.scala
@@ -0,0 +1,182 @@
+package org.senkbeil.debugger.api.utils
+
+import java.util.concurrent.ConcurrentHashMap
+
+import scala.collection.JavaConverters._
+
+/**
+ * Represents a data structure of mappings for multiple values.
+ *
+ * @tparam Key The key used to go from Key -> Seq[Id]
+ * @tparam Value The value yielded from Key or any Id
+ */
+class MultiMap[Key, Value] {
+  /** Represents an id to map key to value */
+  type Id = String
+
+  /** Contains mapping of key -> collection of ids */
+  private val keyToIds = new ConcurrentHashMap[Key, Seq[Id]]().asScala
+
+  /** Contains mapping of id -> value */
+  private val idToValue = new ConcurrentHashMap[Id, Value]().asScala
+
+  /**
+   * Retrieves all keys stored in this map.
+   *
+   * @return The collection of keys
+   */
+  def keys: Seq[Key] = keyToIds.keySet.toSeq
+
+  /**
+   * Retrieves all underlying ids stored in this map used to link
+   * keys to values.
+   *
+   * @return The collection of ids
+   */
+  def ids: Seq[Id] = idToValue.keySet.toSeq
+
+  /**
+   * Retrieves all values stored in this map.
+   *
+   * @return The collection of values
+   */
+  def values: Seq[Value] = idToValue.values.toSeq
+
+  /**
+   * Adds the value with the specified key to the map. Uses the specified id
+   * to link the key with the value.
+   *
+   * @param id The id to use to link the key with the value
+   * @param key The key to use when looking up and removing the value
+   * @param value The value to store in the map
+   *
+   * @return The underlying id used to link the key with the value
+   */
+  def putWithId(id: Id, key: Key, value: Value): Id = {
+    require(!idToValue.keySet.contains(id), "Id must be unique!")
+
+    val oldIds = keyToIds.getOrElseUpdate(key, Nil)
+    keyToIds.put(key, oldIds :+ id)
+
+    idToValue.put(id, value)
+
+    id
+  }
+
+  /**
+   * Adds the value with the specified key to the map.
+   *
+   * @param key The key to use when looking up and removing the value
+   * @param value The value to store in the map
+   *
+   * @return The underlying id used to link the key with the value
+   */
+  def put(key: Key, value: Value): Id = {
+    putWithId(newId(), key, value)
+  }
+
+  /**
+   * Removes the values with the specified key. Also, removes the underlying
+   * ids linking the key to the collection of values.
+   *
+   * @param key The key of the values to remove
+   *
+   * @return Some collection of values if the key exists, otherwise None
+   */
+  def remove(key: Key): Option[Seq[Value]] = {
+    keyToIds.remove(key).map(_.flatMap(idToValue.remove(_: Id)))
+  }
+
+  /**
+   * Removes the value with the specified id. Also, removes the association
+   * between a key and the value via the id.
+   *
+   * @param id The id of the value to remove
+   *
+   * @return Some value if the id exists, otherwise None
+   */
+  def removeWithId(id: Id): Option[Value] = {
+    val removedValue = idToValue.remove(id)
+
+    // Reverse-lookup and remove id from key
+    keyToIds.mapValues(_.filterNot(_ == id)).foreach { case (key, value) =>
+      if (value.nonEmpty) keyToIds.put(key, value)
+      else keyToIds.remove(key)
+    }
+
+    removedValue
+  }
+
+  /**
+   * Determines if the specified key exists in the map.
+   *
+   * @param key The key to check
+   *
+   * @return True if the key exists, otherwise false
+   */
+  def has(key: Key): Boolean = {
+    get(key).nonEmpty
+  }
+
+  /**
+   * Determines if the specified id exists in the map.
+   *
+   * @param id The id to check
+   *
+   * @return True if the id exists, otherwise false
+   */
+  def hasWithId(id: Id): Boolean = {
+    getWithId(id).nonEmpty
+  }
+
+  /**
+   * Retrieves all values for the specified key.
+   *
+   * @param key The key whose values to retrieve
+   *
+   * @return Some collection of values if the key exists, otherwise None
+   */
+  def get(key: Key): Option[Seq[Value]] = {
+    keyToIds.get(key).map(_.flatMap(getWithId))
+  }
+
+  /**
+   * Retrieves the value for the specified id.
+   *
+   * @param id The id of the value to retrieve
+   *
+   * @return Some value if the id exists, otherwise None
+   */
+  def getWithId(id: Id): Option[Value] = {
+    idToValue.get(id)
+  }
+
+  /**
+   * Retrieves the collection of ids for an key.
+   *
+   * @param key The key whose ids to retrieve
+   *
+   * @return Some collection of ids if the key exists, otherwise None
+   */
+  def getIdsWithKey(key: Key): Option[Seq[Id]] = {
+    keyToIds.get(key)
+  }
+
+  /**
+   * Retrieves the key that uses the specified id to map to a value.
+   *
+   * @param id The id used to associate a value with the desired key
+   *
+   * @return Some key if the id exists, otherwise None
+   */
+  def getKeyWithId(id: Id): Option[Key] = {
+    keyToIds.find(_._2.contains(id)).map(_._1)
+  }
+
+  /**
+   * Generates a new id.
+   *
+   * @return The new id from a UUID
+   */
+  protected def newId(): Id = java.util.UUID.randomUUID().toString
+}
---
+++ scala-debugger-api/src/test/scala/org/senkbeil/debugger/api/utils/MultiMapSpec.scala
@@ -0,0 +1,347 @@
+package org.senkbeil.debugger.api.utils
+
+import org.scalatest.{FunSpec, Matchers, OneInstancePerTest}
+
+class MultiMapSpec extends FunSpec with Matchers with OneInstancePerTest {
+
+  private val TestId = java.util.UUID.randomUUID().toString
+  private val TestKey = (java.util.UUID.randomUUID().toString, 999)
+  private val TestValue = java.util.UUID.randomUUID().toString
+
+  private val multiMap = new MultiMap[(String, Int), String]
+
+  describe("MultiMap") {
+    describe("#put") {
+      it("should store the value to be accessible via the key") {
+        val expected = TestValue
+
+        multiMap.put(TestKey, TestValue)
+
+        val actual = multiMap.get(TestKey).get.head
+
+        actual should be (expected)
+      }
+
+      it("should generate a new id to use for mapping the key to the value") {
+        val expected = TestValue
+
+        val id = multiMap.put(TestKey, TestValue)
+
+        val actual = multiMap.getWithId(id).get
+
+        actual should be (expected)
+      }
+
+      it("should add the value to the collection for an existing key") {
+        val expected = Seq(TestValue, TestValue + 1, TestValue + 2)
+
+        expected.foreach(multiMap.put(TestKey, _: String))
+
+        val actual = multiMap.get(TestKey).get
+
+        actual should be (expected)
+      }
+    }
+
+    describe("#putWithId") {
+      it("should store the value to be accessible via the key") {
+        val expected = TestValue
+
+        multiMap.putWithId(TestId, TestKey, TestValue)
+
+        val actual = multiMap.get(TestKey).get.head
+
+        actual should be (expected)
+      }
+
+      it("should use the id provided for mapping the key to the value") {
+        val expected = TestValue
+
+        multiMap.putWithId(TestId, TestKey, TestValue)
+
+        val actual = multiMap.getWithId(TestId).get
+
+        actual should be (expected)
+      }
+
+      it("should throw an exception if the id has already been used") {
+        multiMap.putWithId(TestId, TestKey, TestValue)
+
+        intercept[IllegalArgumentException] {
+          multiMap.putWithId(TestId, TestKey, TestValue)
+        }
+      }
+
+      it("should return the id provided") {
+        val expected = TestId
+
+        val actual = multiMap.putWithId(TestId, TestKey, TestValue)
+
+        actual should be (expected)
+      }
+
+      it("should add the value to the collection for an existing key") {
+        val expected = Seq(TestValue, TestValue + 1, TestValue + 2)
+
+        expected.foreach(multiMap.putWithId(
+          java.util.UUID.randomUUID().toString,
+          TestKey,
+          _: String
+        ))
+
+        val actual = multiMap.get(TestKey).get
+
+        actual should be (expected)
+      }
+    }
+
+    describe("#has") {
+      it("should return true if the key exists") {
+        val expected = true
+
+        multiMap.put(TestKey, TestValue)
+
+        val actual = multiMap.has(TestKey)
+
+        actual should be (expected)
+      }
+
+      it("should return false if the key does not exist") {
+        val expected = false
+
+        val actual = multiMap.has(TestKey)
+
+        actual should be (expected)
+      }
+    }
+
+    describe("#hasWithId") {
+      it("should return true if the id exists") {
+        val expected = true
+
+        multiMap.putWithId(TestId, TestKey, TestValue)
+
+        val actual = multiMap.hasWithId(TestId)
+
+        actual should be (expected)
+      }
+
+      it("should return false if the id does not exist") {
+        val expected = false
+
+        val actual = multiMap.hasWithId(TestId)
+
+        actual should be (expected)
+      }
+    }
+
+    describe("#get") {
+      it("should return Some(collection of values) if the key exists") {
+        val expected = Some(Seq(TestValue, TestValue + 1, TestValue + 2))
+
+        expected.get.foreach(multiMap.put(TestKey, _: String))
+
+        val actual = multiMap.get(TestKey)
+
+        actual should be (expected)
+      }
+
+      it("should return None if the key does not exist") {
+        val expected = None
+
+        val actual = multiMap.get(TestKey)
+
+        actual should be (expected)
+      }
+    }
+
+    describe("#getWithId") {
+      it("should return Some(value) if the id exists") {
+        val expected = Some(TestValue)
+
+        multiMap.putWithId(TestId, TestKey, expected.get)
+
+        val actual = multiMap.getWithId(TestId)
+
+        actual should be (expected)
+      }
+
+      it("should return None if the id does not exist") {
+        val expected = None
+
+        val actual = multiMap.getWithId(TestId)
+
+        actual should be (expected)
+      }
+    }
+
+    describe("#remove") {
+      it("should remove the key if it exists along with all associated ids") {
+        val ids = Seq(TestId, TestId + 1, TestId + 2)
+
+        ids.foreach(multiMap.putWithId(_: String, TestKey, TestValue))
+
+        multiMap.remove(TestKey)
+
+        // Key should no longer exist
+        multiMap.get(TestKey) should be (None)
+
+        // None of the ids should exist
+        ids.foreach { id =>
+          multiMap.getWithId(id) should be (None)
+        }
+      }
+
+      it("should return Some(collection of removed values) if the key exists") {
+        val expected = Some(Seq(TestValue, TestValue + 1, TestValue + 2))
+
+        expected.get.foreach(multiMap.put(TestKey, _: String))
+
+        val actual = multiMap.remove(TestKey)
+
+        actual should be (expected)
+      }
+
+      it("should return None if the key does not exist") {
+        val expected = None
+
+        val actual = multiMap.remove(TestKey)
+
+        actual should be (expected)
+      }
+    }
+
+    describe("#removeWithId") {
+      it("should remove the id from the map") {
+        multiMap.putWithId(TestId, TestKey, TestValue)
+
+        multiMap.removeWithId(TestId)
+
+        multiMap.getWithId(TestId) should be (None)
+      }
+
+      it("should remove the association between a key and the id") {
+        multiMap.putWithId(TestId, TestKey, TestValue)
+        multiMap.putWithId(TestId + 1, TestKey, TestValue)
+
+        multiMap.removeWithId(TestId)
+
+        multiMap.getIdsWithKey(TestKey).get should not contain (TestId)
+      }
+
+      it("should remove the key if the removed id was the only associated id") {
+        multiMap.putWithId(TestId, TestKey, TestValue)
+
+        multiMap.removeWithId(TestId)
+
+        multiMap.getIdsWithKey(TestKey) should be (None)
+      }
+
+      it("should return Some(value) if the id exists") {
+        val expected = Some(TestValue)
+
+        multiMap.putWithId(TestId, TestKey, TestValue)
+
+        val actual = multiMap.removeWithId(TestId)
+
+        actual should be (expected)
+      }
+
+      it("should return None if the id does not exist") {
+        val expected = None
+
+        val actual = multiMap.removeWithId(TestId)
+
+        actual should be (expected)
+      }
+    }
+
+    describe("#getIdsForKey") {
+      it("should return Some(collection of ids) if the key exists") {
+        val expected = Some(Seq(TestId, TestId + 1, TestId + 2))
+
+        expected.get.foreach(multiMap.putWithId(_: String, TestKey, TestValue))
+
+        val actual = multiMap.getIdsWithKey(TestKey)
+
+        actual should be (expected)
+      }
+
+      it("should return None if the key does not exist") {
+        val expected = None
+
+        val actual = multiMap.getIdsWithKey(TestKey)
+
+        actual should be (expected)
+      }
+    }
+
+    describe("#getKeyWithId") {
+      it("should return Some(key) if the id exists") {
+        val expected = Some(TestKey)
+
+        multiMap.putWithId(TestId, TestKey, TestValue)
+
+        val actual = multiMap.getKeyWithId(TestId)
+
+        actual should be (expected)
+      }
+
+      it("should return None if the id does not exist") {
+        val expected = None
+
+        val actual = multiMap.getKeyWithId(TestId)
+
+        actual should be (expected)
+      }
+    }
+
+    describe("#keys") {
+      it("should return a collection of all keys in the map") {
+        val expected = Seq(TestKey, TestKey.copy(_2 = 0), TestKey.copy(_2 = 1))
+
+        expected.foreach(multiMap.put(_: (String, Int), TestValue))
+
+        val actual = multiMap.keys
+
+        actual should contain theSameElementsAs (expected)
+      }
+
+      it("should be empty if the map is empty") {
+        multiMap.keys should be (empty)
+      }
+    }
+
+    describe("#ids") {
+      it("should return a collection of all ids in the map") {
+        val expected = Seq(TestId, TestId + 1, TestId + 2)
+
+        expected.foreach(multiMap.putWithId(_: String, TestKey, TestValue))
+
+        val actual = multiMap.ids
+
+        actual should contain theSameElementsAs (expected)
+      }
+
+      it("should be empty if the map is empty") {
+        multiMap.ids should be (empty)
+      }
+    }
+
+    describe("#values") {
+      it("should return a collection of all values in the map") {
+        val expected = Seq(TestValue, TestValue + 1, TestValue + 2)
+
+        expected.foreach(multiMap.put(TestKey, _: String))
+
+        val actual = multiMap.values
+
+        actual should contain theSameElementsAs (expected)
+      }
+
+      it("should be empty if the map is empty") {
+        multiMap.values should be (empty)
+      }
+    }
+  }
+}
