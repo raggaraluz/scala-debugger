24c7d2697caf537bbb92a29bfcdd00a9f4b715e5
Chip Senkbeil <chip.senkbeil@gmail.com>
2015-12-11 21:29:02 -0600

    Added PendingClassPrepareSupport

---
+++ scala-debugger-api/src/main/scala/org/senkbeil/debugger/api/lowlevel/classes/PendingClassPrepareSupport.scala
@@ -0,0 +1,105 @@
+package org.senkbeil.debugger.api.lowlevel.classes
+
+import org.senkbeil.debugger.api.lowlevel.requests.JDIRequestArgument
+import org.senkbeil.debugger.api.utils.PendingActionManager
+
+import scala.util.{Failure, Success, Try}
+
+/**
+ * Provides pending class prepare capabilities to an existing
+ * class prepare manager.
+ */
+trait PendingClassPrepareSupport extends ClassPrepareManager {
+  /**
+   * Represents the manager used to store pending class prepare requests and
+   * process them later.
+   */
+  protected val pendingActionManager: PendingActionManager[ClassPrepareRequestInfo]
+
+  /** When enabled, results in adding any failed request as pending. */
+  @volatile var enablePending: Boolean = true
+
+  /**
+   * Processes all pending class prepare requests.
+   *
+   * @return The collection of successfully-processed class prepare requests
+   */
+  def processAllPendingClassPrepareRequests(): Seq[ClassPrepareRequestInfo] = {
+    pendingActionManager.processAllActions().map(_.data)
+  }
+
+  /**
+   * Retrieves a list of pending class prepare requests.
+   *
+   * @return The collection of class prepare requests
+   */
+  def pendingClassPrepareRequests: Seq[ClassPrepareRequestInfo] = {
+    pendingActionManager.getPendingActionData(_ => true)
+  }
+
+  /**
+   * Creates a new class prepare request.
+   *
+   * @param extraArguments Any additional arguments to provide to the request
+   *
+   * @return Success(id) if successful, otherwise Failure
+   */
+  abstract override def createClassPrepareRequest(
+    extraArguments: JDIRequestArgument*
+  ): Try[String] = createClassPrepareRequestWithId(
+    newRequestId(),
+    extraArguments: _*
+  )
+
+  /**
+   * Creates a new class prepare request.
+   *
+   * @param requestId The id of the request used to retrieve and delete it
+   * @param extraArguments Any additional arguments to provide to the request
+   *
+   * @return Success(id) if successful, otherwise Failure
+   */
+  abstract override def createClassPrepareRequestWithId(
+    requestId: String,
+    extraArguments: JDIRequestArgument*
+  ): Try[String] = {
+    def createClassPrepareRequest() = super.createClassPrepareRequestWithId(
+      requestId,
+      extraArguments: _*
+    )
+
+    val result = createClassPrepareRequest()
+
+    // If failed, add as pending
+    result.recoverWith {
+      case _: Throwable if enablePending =>
+        pendingActionManager.addPendingActionWithId(
+          requestId,
+          ClassPrepareRequestInfo(extraArguments),
+          () => createClassPrepareRequest().get
+        )
+        Success(requestId)
+      case throwable: Throwable if !enablePending =>
+        Failure(throwable)
+    }
+  }
+
+  /**
+   * Removes the specified class prepare request.
+   *
+   * @param id The id of the Class Prepare Request
+   *
+   * @return True if the class prepare request was removed (if it existed),
+   *         otherwise false
+   */
+  abstract override def removeClassPrepareRequest(id: String): Boolean = {
+    val result = super.removeClassPrepareRequest(id)
+
+    val pendingResult = pendingActionManager.removePendingActionsWithId(id)
+
+    // True if we removed a real class prepare request or any
+    // pending class prepare request
+    result || pendingResult.getOrElse(Nil).nonEmpty
+  }
+}
+
---
+++ scala-debugger-api/src/test/scala/test/TestClassPrepareManager.scala
@@ -0,0 +1,33 @@
+package test
+
+import com.sun.jdi.request.ClassPrepareRequest
+import org.senkbeil.debugger.api.lowlevel.classes.{ClassPrepareRequestInfo, ClassPrepareManager}
+import org.senkbeil.debugger.api.lowlevel.requests.JDIRequestArgument
+
+import scala.util.Try
+
+/**
+ * Test class prepare manager that merely invokes the provided
+ * class prepare manager underneath to make it easier to mock.
+ *
+ * @param classPrepareManager The underlying class prepare manager used to
+ *                            execute all methods
+ */
+class TestClassPrepareManager(
+  private val classPrepareManager: ClassPrepareManager
+) extends ClassPrepareManager {
+  override def classPrepareRequestList: Seq[String] =
+    classPrepareManager.classPrepareRequestList
+  override def createClassPrepareRequest(extraArguments: JDIRequestArgument*): Try[String] =
+    classPrepareManager.createClassPrepareRequest(extraArguments: _*)
+  override def hasClassPrepareRequest(id: String): Boolean =
+    classPrepareManager.hasClassPrepareRequest(id)
+  override def getClassPrepareRequest(id: String): Option[ClassPrepareRequest] =
+    classPrepareManager.getClassPrepareRequest(id)
+  override def createClassPrepareRequestWithId(requestId: String, extraArguments: JDIRequestArgument*): Try[String] =
+    classPrepareManager.createClassPrepareRequestWithId(requestId, extraArguments: _*)
+  override def removeClassPrepareRequest(id: String): Boolean =
+    classPrepareManager.removeClassPrepareRequest(id)
+  override def getClassPrepareRequestInfo(id: String): Option[ClassPrepareRequestInfo] =
+    classPrepareManager.getClassPrepareRequestInfo(id)
+}
