22fc1ede603821affeca94c5052881b00fbbfc23
Chip Senkbeil <chip.senkbeil@gmail.com>
2017-08-30 14:45:48 -0500

    Updated head with facebook pixel snippet

--- scala-debugger-docs/src/main/scala/org/scaladebugger/docs/layouts/SitePage.scala
+++ scala-debugger-docs/src/main/scala/org/scaladebugger/docs/layouts/SitePage.scala
@@ -10,6 +10,8 @@ import org.senkbeil.grus.layouts.{Context, Page}
 import scalatags.Text.all._
 import org.scaladebugger.docs.styles.Implicits._
 
+import scalatags.Text
+
 /**
  * Represents the layout for a common site page.
  *
@@ -22,6 +24,8 @@ abstract class SitePage(
   val selectedMenuItems: Seq[String] = Nil,
   val syntaxHighlightTheme: String = "agate"
 ) extends Page {
+  private val pixelId: String = "1622567071086905"
+
   override protected def preHeadContent(context: Context): Seq[Modifier] =
     super.preHeadContent(context) ++ Seq(
       FontAwesomeCSS(),
@@ -39,6 +43,30 @@ abstract class SitePage(
           MainMenu(context.mainMenuItems: _*)
         )
       )
+    )
+
+  override protected def postHeadContent(context: Context): Seq[Text.all.Modifier] =
+    super.postHeadContent(context) ++ Seq(
+      script(
+        raw(s"""
+           |  !function(f,b,e,v,n,t,s)
+           |  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+           |  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+           |  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+           |  n.queue=[];t=b.createElement(e);t.async=!0;
+           |  t.src=v;s=b.getElementsByTagName(e)[0];
+           |  s.parentNode.insertBefore(t,s)}(window, document,'script',
+           |  'https://connect.facebook.net/en_US/fbevents.js');
+           |  fbq('init', '$pixelId');
+           |  fbq('track', 'PageView');
+        """.stripMargin)
+      ),
+      tag("noscript")(img(
+        height := "1",
+        width := "1",
+        style := "display: none;",
+        src := s"https://www.facebook.com/tr?id=$pixelId&ev=PageView&noscript=1"
+      )())
     )
 
   override protected def postBodyContent(context: Context): Seq[Modifier] =
